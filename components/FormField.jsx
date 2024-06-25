ebc84d96991262718aaf3d66384d7d036ecee7a1
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2024-06-25 19:25:44 +0300

    styled the Form field for password

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,6 +1,6 @@
-import { View, Text, TextInput } from "react-native";
-import React from "react";
-
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { useState } from "react";
+import { icons } from "../constants";
 const FormField = ({
     title,
     value,
@@ -9,13 +9,30 @@ const FormField = ({
     otherStyles,
     ...props
 }) => {
+    const [showPassword, setShowPassword] = useState(false);
     return (
         <View className={`${otherStyles} space-y-2`}>
             <Text className="text-base text-gray-100 font-pmedium">
                 {title}
             </Text>
             <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center">
-                <TextInput />
+                <TextInput
+                    className="flex-1 text-white font-psemibold text-base"
+                    value={value}
+                    placeholder={placeholder}
+                    placeholderTextColor="#7b7b8b"
+                    onChangeText={handleChangeText}
+                    secureTextEntry={title === "Password" && !showPassword}
+                />
+                {title === "Password" && (
+                    <TouchableOpacity
+                        onPress={() => setShowPassword(!showPassword)}
+                    >
+                        <Image
+                            source={!showPassword ? icons.eye : icons.eyeHide}
+                        />
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     );
